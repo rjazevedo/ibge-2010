290ae683ad9546052d3c4acea175b72fddd02aa3
Rafael de Oliveira Werneck <werneck@Guaraci.local>
2024-04-10 10:21:36 -0300

    Melhorando comentários

--- ibge_functions.py
+++ ibge_functions.py
@@ -17,7 +17,7 @@ import subprocess
 
 
 
-# import da classe principal
+# import da classe principal IBGE Parser
 from ibgeparser.microdados import Microdados
 # import dos enums para facilitar as buscas
 from ibgeparser.enums import Anos, Estados, Modalidades
@@ -34,7 +34,7 @@ def function_obterdados_especificacao_coluna(ano_ac, estados_ac, modalidades_ac)
 def Filtrar_Dados_Censo(path,name,i):
 
     file = path + name
-    X = pd.read_csv(file,usecols=["V6036", "V6400", "V6352", "V6354", "V6356", "V6461",  "V6471", "V6462", "V6472","V6511","V6514","V0601","V0656"],sep=",")    
+    X = pd.read_csv(file,usecols=["V6036", "V6400", "V6352", "V6354", "V6356", "V6461",  "V6471", "V6462", "V6472","V6511","V6514","V0601","V0656"],sep=",")
     dict = {
             "V6036":"Idade_em_Anos",
             "V6400":"Nível_instrução",
@@ -51,28 +51,28 @@ def Filtrar_Dados_Censo(path,name,i):
             "V0656":"rendimento_aposentadoria_pensao"
             }
     X.rename(columns=dict,inplace=True)
-        
+
     name_path = name.split(".csv")
     path_proc = ['/home/essantos/Downloads/ibge-2010/processados/Sul/', '/home/essantos/Downloads/ibge-2010/processados/Centro_Oeste/']
     name_path = path_proc[i] + name_path[0] + "_Fase1.csv"
-    X.to_csv(name_path) 
+    X.to_csv(name_path)
     return X
 
 def Limpeza_Arquivo_Censo_Graduados_NaoGraduados_1_2(path,name,i):
 
     file = path + name
-    X = pd.read_csv(file, sep=",")  
+    X = pd.read_csv(file, sep=",")
     X = X.drop(columns=['Unnamed: 0'])
-    
+
     print("Linhas faltantes:==============================")
     print(X.isnull().sum())
 
     #removendo quem não tem ocupação
     X = X.dropna(subset=['Ocupação_Código'])
-    
+
     #removendo pessoas com ocupações mal-definidas
     X.drop(X[(X['Ocupação_Código'] <1)].index, inplace=True)
-    
+
 
     print("Listando os NANs que ainda restam:==============================")
     print(X.isnull().sum())
@@ -82,30 +82,30 @@ def Limpeza_Arquivo_Censo_Graduados_NaoGraduados_1_2(path,name,i):
 
     #Removendo CBO-Domiciliar
     X = X.drop(columns=['CBO-Domiciliar'])
-    
+
     # removendo que tem graduação, mas o curso superior é igual  a Zero
     X.drop(X[(X['Nível_instrução'] ==4) & (X['Curso_Superior_Graduação_Código'] ==0)].index, inplace=True) ## alterado 23/09/2023 #=============================
-   
+
 
     name_path = name.split(".csv")
     path_proc = ['/home/essantos/Downloads/ibge-2010/processados/Sul/Graduados_NaoGraduados/', '/home/essantos/Downloads/ibge-2010/processados/Centro_Oeste/Graduados_NaoGraduados/']
     name_path = path_proc[i] + name_path[0] + "_Graduados_NaoGraduados.csv"
-    X.to_csv(name_path) 
-    return   
+    X.to_csv(name_path)
+    return
 
 #https://colab.research.google.com/drive/16TrgyaIq6T0fbGKl9gOWBXxbAIUfJtCD?authuser=1#scrollTo=qXidkc7VxkIT
 #https://colab.research.google.com/drive/1byICdSAZxE2L8mS5NYpVjMcxKSqvlPUo?authuser=1#scrollTo=SQYQRsulgWpt
 def Pivot_Table_Censo(path,name,gender,i):
     #...
     if gender == "M":
-        
+
         file = path + name
-        X= pd.read_csv(file,usecols=["Nível_instrução", "Ocupação_Código", "gênero"], sep=",")  	
+        X= pd.read_csv(file,usecols=["Nível_instrução", "Ocupação_Código", "gênero"], sep=",")
 
         #Novo Filtro
         #removendo pessoas do sexo feminino ...
         X.drop(X[(X['gênero'] ==2)].index, inplace=True)
-        
+
         X_1 = Pivot_Table(X)
 
         name_path = name.split(".csv")
@@ -115,9 +115,9 @@ def Pivot_Table_Censo(path,name,gender,i):
         X_1.to_csv(name_path)
     else:
         if gender == "F":
-           
+
            file = path + name
-           X= pd.read_csv(file,usecols=["Nível_instrução", "Ocupação_Código", "gênero"], sep=",")  	
+           X= pd.read_csv(file,usecols=["Nível_instrução", "Ocupação_Código", "gênero"], sep=",")
 
            #Novo Filtro
            #removendo pessoas do sexo masculino ...
@@ -132,7 +132,7 @@ def Pivot_Table_Censo(path,name,gender,i):
            X_1.to_csv(name_path)
         else:
              file = path + name
-             X= pd.read_csv(file,usecols=["Nível_instrução", "Ocupação_Código", "gênero"], sep=",")  	
+             X= pd.read_csv(file,usecols=["Nível_instrução", "Ocupação_Código", "gênero"], sep=",")
 
              X_1 = Pivot_Table(X)
 
@@ -141,36 +141,36 @@ def Pivot_Table_Censo(path,name,gender,i):
              #name_path = name_path[0] + "_PivotTablet.csv"
              name_path = '/home/essantos/Downloads/ibge-2010/processados/temp/'+ name_path[0] + "_PivotTablet.csv"
              X_1.to_csv(name_path)
-    return 
+    return
 
 def Pivot_Table(X):
-    
+
     X[["Nível_instrução", "Ocupação_Código"]]
 
     # Método que verifica, e soma os valores nulos em todo dataset---------------------------------------------------------------------------------------------------------------
     X.isnull().sum()
 
     #Remover todas as linhas que tem CBO = Nan ---------------------------------------------------------------------------------------------------------------------------------
-    X = X.dropna(subset=['Ocupação_Código'])        
+    X = X.dropna(subset=['Ocupação_Código'])
 
     #Gerando a Pivot table
     X['Ensino Superior']=1
-    X_Pivot= pd.pivot_table(X, values=['Ensino Superior'], index=['Ocupação_Código'],columns=['Nível_instrução'],aggfunc='count',fill_value=0) 
+    X_Pivot= pd.pivot_table(X, values=['Ensino Superior'], index=['Ocupação_Código'],columns=['Nível_instrução'],aggfunc='count',fill_value=0)
     return X_Pivot
 
-def Limpeza_Arquivo_Censo_Graduados_2(path,name,i):     
+def Limpeza_Arquivo_Censo_Graduados_2(path,name,i):
 
     file = path + name
-    X = pd.read_csv(file, sep=",")  
+    X = pd.read_csv(file, sep=",")
     X = X.drop(columns=['Unnamed: 0'])
- 
+
     # Deixando somente os graduados ...
     X.drop(X[(X['Curso_Superior_Graduação_Código'] ==0)].index, inplace=True) #Essa condição, deixa o dataset somente com as pessoas graduadas ...
-    
+
     name_path = name.split(".csv")
     path_proc = ['/home/essantos/Downloads/ibge-2010/processados/Sul/Clean_Graduados/', '/home/essantos/Downloads/ibge-2010/processados/Centro_Oeste/Clean_Graduados/']
     name_path = path_proc[i] + name_path[0] + "_QtdadeSal_SoGraduados.csv"
-    X.to_csv(name_path) 
+    X.to_csv(name_path)
     return
 
 def df2pivot(df):
@@ -187,25 +187,25 @@ def Reduzir(pivot_final,estado,gender):
     #...
     #pivotfinal = reduce(lambda a, b: a.add(b, fill_value=0), [pivot_final[0], pivot_final[1]])
     pivotfinal = reduce(lambda a, b: a.add(b, fill_value=0), pivot_final)
-   
+
     if gender ==1:
        name_path = '/home/essantos/Downloads/ibge-2010/processados/CSVs_PivotTableFinal/' + estado + '_PivotFinalMasculina.csv'
-       pivotfinal.to_csv(name_path)    
+       pivotfinal.to_csv(name_path)
     else:
          if gender ==2:
             name_path = '/home/essantos/Downloads/ibge-2010/processados/CSVs_PivotTableFinal/' + estado + '_PivotFinalFeminina.csv'
-            pivotfinal.to_csv(name_path)   
+            pivotfinal.to_csv(name_path)
          else:
               if gender ==3:
                  name_path = '/home/essantos/Downloads/ibge-2010/processados/CSVs_PivotTableFinal/' + estado + '_PivotFinal.csv'
-                 pivotfinal.to_csv(name_path)          
+                 pivotfinal.to_csv(name_path)
     return
 
 def SomaPivotTable(path,name,i):
     #...
     file = path + name
-    pivot = pd.read_csv(file)  
-    pivot = df2pivot(pivot) 
+    pivot = pd.read_csv(file)
+    pivot = df2pivot(pivot)
     return pivot
 
 def Soma_PivotTableFinal():
@@ -218,7 +218,7 @@ def JuntarCSVs(path,estado,opcao):
     import pandas as pd
 
     os.chdir(path)
-    
+
     extension = 'csv'
     all_filenames = [i for i in glob.glob('*.{}'.format(extension))]
 
@@ -235,9 +235,9 @@ def JuntarCSVs(path,estado,opcao):
            name_path = name.split(".csv")
            name_path = '/home/essantos/Downloads/ibge-2010/processados/temp/'+ name_path[0] + "_Não-Graduados.csv"
            combined_csv.to_csv(name_path, index=False, encoding='utf-8-sig')
-        
 
-    return 
+
+    return
 
 
 def move_temp(estado, opcao):
@@ -249,7 +249,7 @@ def move_temp(estado, opcao):
           comando = "chmod +x /home/essantos/Downloads/ibge-2010/mv.sh"
           subprocess.check_output(comando, shell=True)
           comando = "/home/essantos/Downloads/ibge-2010/mv.sh"
-          subprocess.check_output(comando, shell=True)          
+          subprocess.check_output(comando, shell=True)
        else:
             if estado == "Centro_Oeste":
                print("Centro_Oeste")
