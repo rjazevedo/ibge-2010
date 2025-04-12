ebc1fe5dcdf57b1dba5f240440ed8f0db1fea680
elis-essantos <e072207@gmail.com>
2025-04-12 12:33:12 +0000

    cluster - novo arquivo

---
+++ ibge_results_1.py
@@ -0,0 +1,24 @@
+#pip install ibge-parser
+
+import string
+import sys
+import ibgeparser
+from pandas import *
+import pandas as pd
+from pandas import DataFrame
+import numpy as np
+import os
+import glob
+from functools import reduce
+from os import chmod
+from copy import deepcopy
+import subprocess
+import glob
+# import da classe principal
+from ibgeparser.microdados import Microdados
+# import dos enums para facilitar as buscas
+from ibgeparser.enums import Anos, Estados, Modalidades
+import ibge_variable
+import matplotlib.pyplot as plt
+import logging
+import ibge_functions_descriptive_analysis
